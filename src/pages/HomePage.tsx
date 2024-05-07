4909896b44e871f6b81d59bb7fefe22c8ab0609e
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-05-08 00:35:17 +0530

    homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import Footer from "../components/Footer"
 import HeroSection from "../components/HeroSection"
 import Navbar from "../components/Navbar"
+import Questionaire from "../components/Questionaire"
 
 const HomePage = () => {
   return (
     <div className="font-inter">
         <Navbar />
         <HeroSection />
+        <Questionaire />
         <Footer />
     </div>
   )
